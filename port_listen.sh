f4511da44c29b7a3aab9a1bb015d2657ef91e484
Tuan T. Pham <tuan@vt.edu>
2019-07-15 09:22:10 -0400

    Add support for clean exit
    port_listen.sh:
        * Use signal to exit cleanly when we get message before timeout
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- port_listen.sh
+++ port_listen.sh
@@ -7,22 +7,55 @@ TIMEOUT=${TIMEOUT:=5}
 PORT_NUMBER=${PORT_NUMER:=3000}
 LISTEN_APP=${LISTEN_APP:=nc}
 APP_OPTS=${APP_OPTS:="-l"}
+APP_PID=0
+RECV_MSG=0
 
-which ${LISTEN_APP} >/dev/null
-if [[ $? != 0 ]]; then
-    echo "${LISTEN_APP} does not exist!"
-    exit 1
-fi
+function check_app()
+{
+    which ${LISTEN_APP} >/dev/null
+    if [[ $? != 0 ]]; then
+        echo "${LISTEN_APP} does not exist!"
+        exit 1
+    fi
+}
 
-${LISTEN_APP} ${APP_OPTS} ${PORT_NUMBER} &
-APP_PID=$!
+function listen_port()
+{
+    ${LISTEN_APP} ${APP_OPTS} ${PORT_NUMBER} &
+    APP_PID=$!
+}
 
-sleep ${TIMEOUT}
-kill ${APP_PID} 2>/dev/null
+function wait_pid()
+{
+    wait ${APP_PID} && RECV_MSG=1
+}
 
-if [[ $? == 0 ]]; then
-    exit 1
-else
+function timer()
+{
+    sleep ${TIMEOUT}
+    kill ${APP_PID} 2>/dev/null
+
     exit 0
-fi
+}
 
+function finish()
+{
+    echo "Exit"
+    echo "RECV_MSG=${RECV_MSG}"
+    if [[ $RECV_MSG -eq 1 ]]; then
+        exit 0
+    else
+        exit 1
+    fi
+}
+function main()
+{
+    check_app
+    listen_port
+    timer &
+    echo "Waiting on ${APP_PID}"
+    wait_pid
+}
+
+trap finish EXIT TERM
+main
